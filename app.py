79b1e42850decd57e607c7aaabec2c8fce908114
Diamond427 <allenbull1234@gmail.com>
2024-01-23 05:37:33 -0800

    feature/save&load 1.23

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 import uvicorn
-from fastapi import FastAPI, UploadFile, Form
-from fastapi.responses import StreamingResponse
+from fastapi import FastAPI, HTTPException, UploadFile, Form
+from fastapi.responses import FileResponse, StreamingResponse
 from starlette.middleware.cors import CORSMiddleware
 from schemas import frontProfileSchema, sideProfileSchema, ImageOverviewSchema
+from ReportProcess import ReportStoreSchema
 from datetime import datetime
+from dotenv import load_dotenv
 from pathlib import Path
 import hashlib
 import json
-import io
-import zipfile
 
 import ProfileScoreCalcFront
 import ProfileScoreCalcSide
@@ -20,6 +22,7 @@ import Auth
 import Payment
 
 app = FastAPI()
+load_dotenv()
 
 
 app.add_middleware(
@@ -64,32 +67,85 @@ async def generateImageOverview(
     points: str = Form(...)):
     currentIndex = hashlib.md5(str(datetime.now()).encode()).hexdigest()
 
+    currentDirectory = os.path.dirname(__file__)
+    uploadFolderPath = os.path.join(currentDirectory, os.getenv("UPLOAD_FOLDER"))
+    front_img_name = f"{currentIndex}_front.jpg"
+    side_img_name = f"{currentIndex}_side.jpg"
+    front_img_path = os.path.join(uploadFolderPath, front_img_name)
+    side_img_path = os.path.join(uploadFolderPath, side_img_name)
+
+    print({currentDirectory, uploadFolderPath, front_img_name, side_img_name})
+
     # Save frontImage with currentIndex name
-    frontImgPth = f"UPLOADS/{currentIndex}_0.jpg"
-    with open(frontImgPth, "wb") as f:
+    with open(front_img_path, "wb") as f:
         f.write(front.file.read())
     # Save sideImage with currentIndex name
-    sideImgPth = f"UPLOADS/{currentIndex}_1.jpg"
-    with open(sideImgPth, "wb") as f:
+    with open(side_img_path, "wb") as f:
         f.write(side.file.read())
-    await CreateReportImages.createReportImages(frontImgPth, sideImgPth, json.JSONDecoder().decode(points))
+    await CreateReportImages.createReportImages(front_img_path, side_img_path, json.JSONDecoder().decode(points))    
+    return {"id" : currentIndex}
+    
+@app.get("/get_image/{id}/{image_index}")
+async def get_image(id: str, image_index: int):
+    file_path = f"REPORTS/{id}/{image_index}.jpg"
+    return FileResponse(file_path, media_type="image/jpeg")
 
-    zip_buffer = io.BytesIO()
-    with zipfile.ZipFile(zip_buffer, "w") as zip_file:
-        for i in range(1, 46):
-            image_path = Path(f"REPORTS/{currentIndex}") / f"{i}.jpg"
-            zip_file.write(image_path, arcname=image_path.name)
-            print(image_path)
+@app.get("/uploads/{file_name}")
+async def get_file(file_name: str):
+    file_path = f"UPLOADS/{file_name}"
+    return FileResponse(file_path)
 
-    # Rewind the buffer to the beginning
-    zip_buffer.seek(0)
+@app.post('/save')
+async def saveReport(frontImage:UploadFile = Form(...),
+    sideImage:UploadFile = Form(...),
+    mail: str = Form(...),
+    reportID: str = Form(...),
+    gender: str = Form(...),
+    race: str = Form(...),
+    reportOwner: str = Form(...),
+    keyPoints: list = Form(...)):
 
-    headers = {
-        "Content-Disposition": "attachment; filename=images.zip",
-        "Content-Type": "application/zip",
-    }
+    currentIndex = hashlib.md5(str(datetime.now()).encode()).hexdigest()
 
-    return StreamingResponse(io.BytesIO(zip_buffer.read()), headers=headers)
+    # Save images
+    try:
+        currentDirectory = os.path.dirname(__file__)
+        uploadFolderPath = os.path.join(currentDirectory, os.getenv("UPLOAD_FOLDER"))
+
+        front_img_name = f"{currentIndex}_front.jpg"
+        side_img_name = f"{currentIndex}_side.jpg"
+        front_img_path = os.path.join(uploadFolderPath, front_img_name)
+        side_img_path = os.path.join(uploadFolderPath, side_img_name)
+
+        with open(front_img_path, "wb") as f:
+            f.write(frontImage.file.read())
+        with open(side_img_path, "wb") as f:
+            f.write(sideImage.file.read())
+    except Exception as e:
+        print(f"File saving error {e}")
+    
+    body = ReportStoreSchema(
+        mail = mail,
+        reportID = reportID,
+        gender = gender,
+        race = race,
+        reportOwner = reportOwner,
+        keyPoints = keyPoints,
+        frontImage = front_img_name,
+        sideImage = side_img_name
+        )
+
+    return body.store()
+
+@app.get("/reports/{mail}")
+async def getReportsByEmail(mail: str):
+    return ReportStoreSchema.getReports(mail)
+
+@app.get("/details/{id}")
+async def getDetails(id: str):
+    result = ReportStoreSchema.getDetails(id)
+
+    return result
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
